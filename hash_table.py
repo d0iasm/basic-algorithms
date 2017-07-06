5a5a4e5ac6c40e802c09da29661dd73c58c8af84
Asami Doi <doiasami1219@gmail.com>
2017-07-06 14:44:56 +0900

    update chain method in hash_table

--- hash_table.py
+++ hash_table.py
@@ -2,25 +2,70 @@
 
 # HashTable
 # 衝突回避 チェイン法
-# 時間計算量 挿入 O(1) 探索 O(1) 削除 O(1)
-# 空間計算量
-
-import sys
+# 時間計算量 挿入 O(1) 検索 O(1) 削除 O(1)
+# 空間計算量 O(n+m) n=テーブルの大きさ m=エントリリストの数
 
 class HashTable(object):
+    class Cell(object):
+        def __init__(self, key, value, next=None):
+            self.key = key
+            self.value = value
+            self.next = next
+
+            
     def __init__(self, size):
+        if size == 0: raise Exception("size is invalid value")
         self.size = size
         self.table = [None] * self.size
 
+        
     def _hash(self, key):
         return hash(key) % self.size
+
+    def _get(self, key):
+        index = self._hash(key)
+        current = self.table[index]
+        while current:
+            if current.key == key:
+                return True, current
+            current = current.next
+
+        return False, index
+
     
-
-    def add(self, key, value):
-        self.table[self._hash(key)] = value
-
     def get(self, key):
-        return self.table[self._hash(key)]
+        if key is None: raise Exception("Key is None")
 
+        is_exist, cell = self._get(key)
+        if is_exist: return cell.value
+        return None
+
+    
+    def add(self, key, value):
+        if key is None: raise Exception("Key is None")
+        
+        is_exist, cell = self._get(key)
+        if is_exist:
+            cell.value = value
+        else:
+            new = HashTable.Cell(key, value, self.table[self._hash(key)])
+            self.table[self._hash(key)] = new
+    
+    
     def delete(self, key):
-        self.table[self._hash(key)] = None
+        if key is None: raise Exception("Key is None")
+
+        index = self._hash(key)
+        current = self.table[index]
+
+        if current.key == key:
+            self.table[index] = current.next
+            return
+        
+        while current.next:
+            if current.next.key == key:
+                current.next = current.next.next
+                return
+            current = current.next
+                
+        
--- test_hash_table.py
+++ test_hash_table.py
@@ -3,16 +3,24 @@ from hash_table import HashTable
 
 
 class TestHashTable(unittest.TestCase):
-    def test_hash(self):
-        hash_table = HashTable(10)
+    def test_hash_table(self):
+        hash_table = HashTable(3)
         hash_table.add("key1", "val1")
         hash_table.add("key2", "val2")
-        # hash_table.add("key3", "val3")
+        hash_table.add("key3", "val3")
+        hash_table.add("", "val_blank")
+        hash_table.add("key_none", None)
         hash_table.delete("key2")
         self.assertEqual("val1", hash_table.get("key1"))
+        self.assertEqual("val_blank", hash_table.get(""))
+        self.assertIsNone(hash_table.get("key_none"))
         self.assertIsNone(hash_table.get("key2"))
         self.assertIsNone(hash_table.get("hoge"))
+        self.assertRaises(Exception, hash_table.get, None)
 
+    def test_invalid_size(self):
+        self.assertRaises(Exception, HashTable, 0)
 
+        
 if __name__ == '__main__':
     unittest.main()
