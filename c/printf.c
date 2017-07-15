4b3949f3c4cda6b400b63b625fbedd4610b726b3
Asami Doi <doiasami1219@gmail.com>
2017-07-15 16:12:34 +0900

    fix printf h27

--- c/printf.c
+++ c/printf.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 
 int h29(void){
-  int a=0xf0f0;
-  int b=0x9876;
-  char s[]="Nagoya_Univ";
+  int a = 0xf0f0;
+  int b = 0x9876;
+  char s[] = "Nagoya_Univ";
   printf("%d\n%x\n%d\n%x\n%c\n", a, a<<2, a|b, a^b, s[7]);
   /* 61680 3c3c0 63734 6886 U*/
   return 0;
 }
 
+int h27(void){
+  int a = 0x0f0f;
+  int b = 0x1234;
+  char s[] = "abcd";
+  printf("%d %x %x %x %c \n", a, a<<3, a|b, a^b, s[1]);
+  /* 3855 7878 1f3f 1d3b b */
+  return 0;
+}
+
 int main(void){
   h29();
+  h27();
   return 0;
 }
