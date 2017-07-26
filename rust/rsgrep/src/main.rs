1e867e399f8b26a6c55b826f6a90ac76dbe457da
Asami Doi <doiasami1219@gmail.com>
2017-07-26 21:00:17 +0900

    update getting pattern from argument

--- rust/rsgrep/src/main.rs
+++ rust/rsgrep/src/main.rs
@@ -2,11 +2,32 @@ use std::fs::File;
 use std::io::{BufReader, BufRead};
 use std::env;
 
+extern crate regex;
+use regex::Regex;
+
+
 fn usage() {
     println!("rsgrep PATTERN FILENAME")
 }
 
 fn main() {
+    let pattern = match env::args().nth(1) {
+        Some(pattern) => pattern,
+        None => {
+            usage();
+            return;
+        }
+    };
+
+    let reg = match Regex::new(&pattern) {
+        Ok(reg) => reg,
+        Err(e) => {
+            println!("invalid regexp {}: {}", pattern, e);
+            return;
+        }
+    };
+
+
     let filename = match env::args().nth(2) {
         Some(filename) => filename,
         None => {
@@ -33,6 +54,9 @@ fn main() {
                 return;
             }
         };
-        println!("{}", line);
+
+        if reg.is_match(&line) {
+            println!("{}", line);
+        }
     }
 }
