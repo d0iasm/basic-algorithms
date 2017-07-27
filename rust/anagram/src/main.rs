3ddaf33ef6f9ef76d635eb8728d70de3f6fadc9b
Asami Doi <doiasami1219@gmail.com>
2017-07-27 22:00:57 +0900

    add anagram project

---
+++ rust/anagram/src/main.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
+use std::path::Path;
+
+fn main() {
+    println!("hello, world!")
+    // 実行時にコマンドライン引数として単語を受け取る
+    // let word = std::env::args().nth(1).expect("USAGE: word");
+    // 辞書からAnagram構造体を作る
+    // 多くのUnix環境では、このパスに辞書がある（ない場合は、手で辞書を準備してパスを変えてください）
+    // let table = Anagram::new("/usr/share/dict/words").expect("failed to make table");
+
+    // println!("{:?}", table.find(&word));
+}
